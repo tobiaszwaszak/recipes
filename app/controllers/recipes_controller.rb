bdc7590dbcfcbfa8ed63df8ad1ca4fa4cdda5480
Tobiasz Waszak <tobiaszwaszak@gmail.com>
2023-06-10 15:28:46 +0200

    scope when we search with one ingredient

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,7 @@
 class RecipesController < ApplicationController
   def index
-    head 200
+    recipes = []
+    recipes = Recipe.joins(:ingredients).where("ingredients.name LIKE ?", "%#{params[:name]}%") if params[:name]
+    render json: recipes.to_json, status: :ok
   end
 end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,10 +1,54 @@
 require "rails_helper"
+Rails.application.load_tasks
 
 RSpec.describe "Recipes", type: :request do
   describe "GET /recipes" do
+    subject(:get_recipes) { get("/recipes", params: params) }
+    let(:params) { {} }
+
     it "returns status 200" do
-      get("/recipes")
+      get_recipes
       expect(response.status).to eq(200)
+    end
+
+    context "when there is no chosen ingredient" do
+      it "returns an empty array" do
+        get_recipes
+        expect(JSON.parse(response.body)).to eq([])
+      end
+    end
+
+    context "when there is one ingredient" do
+      before do
+        json_file_path = Rails.root.join('spec', 'fixtures', 'files', 'recipes-en.json.gz')
+
+        stub_request(:get, "https://pennylane-interviewing-assets-20220328.s3.eu-west-1.amazonaws.com/recipes-en.json.gz")
+          .to_return(body: File.open(json_file_path))
+
+        Rake::Task['json_import:import'].invoke
+      end
+
+      after do
+        Rake::Task['json_import:import'].reenable
+      end
+
+      let(:params) do
+        {
+          name: "egg"
+        }
+      end
+
+      it "returns array with expected 'Golden Sweet Cornbread' recipe" do
+        get_recipes
+
+        expect(JSON.parse(response.body).any? { |recipe| recipe["title"] == "Golden Sweet Cornbread" }).to be true
+      end
+
+      it "returns array with expected 'Diana's Hawaiian Bread Rolls' recipe" do
+        get_recipes
+
+        expect(JSON.parse(response.body).any? { |recipe| recipe["title"] == "Diana's Hawaiian Bread Rolls" }).to be true
+      end
     end
   end
 end
