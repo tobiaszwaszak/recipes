ec69303ccd536ca2e567cf63656f4da8e2f76dc9
Tobiasz Waszak <tobiaszwaszak@gmail.com>
2023-06-10 17:36:30 +0200

    update frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
   };
 
   const handleSearch = () => {
-    axios.get('http://localhost:3001/recipes', {
+    axios.get(process.env.REACT_APP_API_ENDPOINT, {
       params: {
         ingredients: ingredients.split(',').map((ingredient) => ingredient.trim()),
       },
@@ -38,7 +38,19 @@ function App() {
         {recipes.map((recipe) => (
           <div key={recipe.id}>
             <h2>{recipe.title}</h2>
-            {/* Display other recipe details */}
+            <p>Cook Time: {recipe.cook_time} minutes</p>
+            <p>Prep Time: {recipe.prep_time} minutes</p>
+            <h3>Ingredients:</h3>
+            <ul>
+              {recipe.ingredients.map((ingredient, index) => (
+                <li key={index}>{ingredient}</li>
+              ))}
+            </ul>
+            <p>Ratings: {recipe.ratings}</p>
+            <p>Cuisine: {recipe.cuisine}</p>
+            <p>Category: {recipe.category}</p>
+            <p>Author: {recipe.author}</p>
+            <img src={recipe.image} alt={recipe.title} />
           </div>
         ))}
       </div>
